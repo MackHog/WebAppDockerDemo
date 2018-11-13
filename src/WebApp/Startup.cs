6cbbffb1eefe1769331597ebd3c75163a108f584
Mackzilla <marcusjhoglund@gmail.com>
2018-11-13 20:58:39 +0100

    initial commit

---
+++ src/WebApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app) =>
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"{System.DateTime.UtcNow} - Hello from Docker!");
+            });
+    }
+}
---
+++ test/WebApp.Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp.Tests
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void When_Called_Complete_Successfully() =>
+            Assert.IsTrue(true);
+
+    }
+}
